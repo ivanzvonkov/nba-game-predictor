42cedeb20561d4a5c4ef22fb7ab06e60562a9d8f
ivanzvonkov <ivan.zvonkov@gmail.com>
2018-11-05 23:02:34 -0500

    making feature

--- classifier.py
+++ classifier.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
+games = 82
+teams = 30
 
 # Loads features from file and returns features dict
 def load_data(filename):
@@ -11,8 +13,6 @@ def load_data(filename):
 
 # load 2014-2015 stats for each team
 def create_14_15_team_csv():
-    games = 82
-    teams = 30
     df = pd.read_csv("nba.games.stats.csv")
     df_1415 = pd.DataFrame().reindex_like(df[0:teams])
     df_1415.drop(columns=["Unnamed: 0","Game","Date","Home", "Opponent", "WINorLOSS"], axis=1, inplace=True)
@@ -31,19 +31,32 @@ def create_14_15_team_csv():
     df_1415.to_csv("nba.team.1415.csv")
     return
 
+# Creates a table of all match ups and result of 14/15 season
 def create_14_15_season_csv():
     df = pd.read_csv("nba.games.stats.csv")
+    keep_columns = ["Team", "Game", "Date", "Home", "Opponent", "WINorLOSS"]
+    df = df[keep_columns]
+    df = df[:games*teams]
+    df.to_csv("nba.season.1415.csv")
     # only leave 14 15 season
     # remove every column but team, home, opponent, WINorLoss
 
+#feature - team 1 stats, team 1 location, team 2 stats, target - team 1  w/l
+def load_features():
+    print 'making feature'
+    df_keys = load_data("nba.season.1415.csv")
+    df_values = load_data("nba.team.1415.csv")
+
+
+
 if __name__ == "__main__":
     print 'Hello'
+
     # Creates csv for average team stats
     #create_14_15_team_csv()
 
     # Figure out most important stats
-    df = load_data("nba.team.1415.csv")
-    #np.histogram(np.array(df["TeamPoints"]))
+    #df = load_data("nba.team.1415.csv")
 
     # Binning data teampoints next by 3 points
 
@@ -52,5 +65,3 @@ if __name__ == "__main__":
     # Training set - 50 games from each team, feature the one team stats vs other, target 1 or 2
     # Testing set - 10 games from each team,
     # Validation - 10 games from each team
-
-    #feature - team 1 stats, team 1 location, team 2 stats, target - team 1  w/l
