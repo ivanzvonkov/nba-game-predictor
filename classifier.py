424ced5d7fef6ac083ebaee5bb9363f8d3adaf78
ivanzvonkov <ivan.zvonkov@gmail.com>
2018-11-12 22:44:57 -0500

    beginning model work

--- classifier.py
+++ classifier.py
@@ -1,15 +1,16 @@
 import time
+#import tensorflow as tf
+import numpy as np
+import time
 import tensorflow as tf
 import numpy as np
+import gzip
+import matplotlib.pyplot as plt
 import pandas as pd
+from tensorflow.python.data import Dataset
 
 games = 82
 teams = 30
-
-# Loads features from file and returns features dict
-def load_data(filename):
-    df = pd.read_csv(filename)
-    return df
 
 # load 2014-2015 stats for each team
 def create_14_15_team_csv():
@@ -41,26 +42,66 @@ def create_14_15_season_csv():
     # only leave 14 15 season
     # remove every column but team, home, opponent, WINorLoss
 
-#feature - team 1 stats, team 1 location target - team 1  w/l
-def create_features_csv():
+# Feature - team 1 stats, team 1 location target - team 1  w/l
+def create_raw_features_csv():
     print 'making feature'
-    df_keys = load_data("nba.season.1415.csv")
-    df_values = load_data("nba.team.1415.csv")
+    pre ="v."
+    df_keys = pd.read_csv("nba.season.1415.csv")
+    df_values = pd.read_csv("nba.team.1415.csv")
+    #opp_df_values = df_values.rename(columns=lambda col_name: pre+col_name)
+
     df = df_keys.merge(df_values, on=["Team"])
+    #df = df.merge(opp_df_values, left_on=["Opponent"], right_on=[pre+"Team"])
+
     df.drop(columns=["Unnamed: 0_x", "Unnamed: 0_y"], axis=1, inplace=True)
-    df.to_csv("features.csv")
+    #df.drop(columns=[pre+"Unnamed: 0", pre+"Team"], axis=1, inplace=True)
+    df.to_csv("raw_features.csv")
 
 # Fix features so they are ready for machine learning
 def feature_engineering():
-    df = pd.read_csv("features.csv")
+    df = pd.read_csv("raw_features.csv")
 
-    # Team
+    # Convert all teams to numbers
     team_names = df["Team"].unique()
     team_names_dict = dict(zip(team_names, range(len(team_names))))
-    df["Team"].applymap(lambda s: team_names_dict.get(s) if s in team_names_dict else s)
+    df = df.replace(team_names_dict)
 
-    # 1 for WIN, 0 for LOSS
+    # Convert Home to 1 or 0
+    df["Home"] = pd.get_dummies(df["Home"])["Home"]
+
+    # Convert WinLoss to 1 or 0
     df['WINorLOSS'] = pd.get_dummies(df['WINorLOSS'])['W']
+    df.drop(columns=["Unnamed: 0"], axis=1, inplace=True)
+
+    corr = df.corr()['WINorLOSS']
+    for key, value in corr.items():
+        if (abs(value) < 0.1):
+            df.drop([key], axis=1, inplace=True)
+
+    df.to_csv("features.csv")
+
+# Loads features and labels
+def load_features_labels():
+    data = pd.read_csv("features.csv")
+    labels = data["WINorLOSS"]
+    data.drop("WINorLOSS", axis=1, inplace=True)
+    return {"data": data}, labels
+
+# Input function used with dnn_classifier returns iterators of features, labels
+def input_function(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features, targets))
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    # Shuffle the data, if specified.
+    if shuffle:
+        ds = ds.shuffle(buffer_size=10000)
+
+    # Return the next batch of data
+    feature, label = ds.make_one_shot_iterator().get_next()
+    return feature, label
+
+
 
 if __name__ == "__main__":
     print 'Hello'
@@ -71,13 +112,15 @@ if __name__ == "__main__":
     # Creates csv for season keys
     #create_14_15_season_csv()
 
-    #Creates features csv
-    #create_features_csv()
+    # Creates features csv
+    #create_raw_features_csv()
 
-    feature_engineering()
+    # Fix feature values
+    #feature_engineering()
 
-    # Figure out most important stats
-    #df = load_data("nba.team.1415.csv")
+
+
+
 
     # Binning data teampoints next by 3 points
 
