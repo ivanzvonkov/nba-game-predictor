c13598437c9805b94072cbcd1624473ecae0396b
ivanzvonkov <ivan.zvonkov@gmail.com>
2019-02-28 22:19:14 -0500

    1415 playoff results ready

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import sys
+
 import tensorflow as tf
 import pandas as pd
 
@@ -36,21 +38,65 @@ class Client:
     def predict_value(self, one_feature):
         prediction_value, prediction_accuracy = self.predict_one(one_feature)
         if prediction_value == 1:
-            winning_team =   self.home_team
-            losing_team = self.away_team
+            winning_team =   self.team1
+            losing_team = self.team2
         else:
-            winning_team = self.away_team
-            losing_team = self.home_team
+            winning_team = self.team2
+            losing_team = self.team1
 
         print 'Guessing ' + winning_team + ' beat '+ losing_team + ' '+ str(
             "%.2f" % prediction_accuracy) + '% accuracy.'
 
         return winning_team
 
+    def team1_wins(self):
+        return self.team1, self.team2
+
+    def team2_wins(self):
+        return self.team2, self.team1
+
+    def predict_value(self, feature1, feature2):
+        prediction_value1, prediction_accuracy1 = self.predict_one(feature1)
+        prediction_value2, prediction_accuracy2 = self.predict_one(feature2)
+
+        if prediction_value1 == 1 and prediction_value2 == 0:
+            w, l = self.team1_wins()
+            accuracy = (prediction_accuracy1 + prediction_accuracy2) / 2
+
+        elif prediction_value1 == 0 and prediction_value2 == 1:
+            w, l = self.team2_wins()
+            accuracy = (prediction_accuracy1 + prediction_accuracy2) / 2
+
+        # both teams win at home
+        elif prediction_value1 ==  1 and prediction_value2 == 1:
+            if prediction_accuracy1 > prediction_accuracy2:
+                w, l = self.team1_wins()
+            else:
+                w, l = self.team2_wins()
+            accuracy = abs(prediction_accuracy1 - prediction_accuracy2)
+
+        # both team win away
+        elif prediction_value1 == 0 and prediction_value2 == 0:
+            if prediction_accuracy1 > prediction_accuracy2:
+                w, l = self.team2_wins()
+            else:
+                w, l = self.team1_wins()
+            accuracy = abs(prediction_accuracy1 - prediction_accuracy2)
+
+        print 'Guessing ' + w + ' beat ' + l + ' with ' + str(
+            "%.2f" % accuracy) + '% confidence.'
+
+        return w
+
     def __init__(self):
 
+        # CHANGE BELOW BASED ON PLAYOFF YEAR
+        # ----------------------------------------------------------------------------
+        sys.stdout = open('playoffs_1415.txt', 'w')
         current_round_west = ['GSW', 'HOU', 'LAC', 'POR', 'MEM', 'SAS', 'DAL', 'NOP']
         current_round_east = ['ATL', 'CLE', 'CHI', 'TOR', 'WAS', 'MIL', 'BOS', 'BRK']
+        # ----------------------------------------------------------------------------
+
         next_round_west = []
         next_round_east = []
 
@@ -65,21 +111,22 @@ class Client:
 
                 # More than one team left in west current round
                 if not west_finalist_chosen and len(current_round_west) > 1:
-                    self.home_team = current_round_west.pop(0)
-                    self.away_team = current_round_west.pop(len(current_round_west)-1)
+                    self.team1 = current_round_west.pop(0)
+                    self.team2 = current_round_west.pop(len(current_round_west)-1)
 
                 # More than one team left in east current round
                 elif not east_finalist_chosen and len(current_round_east) > 1:
-                    self.home_team = current_round_east.pop(0)
-                    self.away_team = current_round_east.pop(len(current_round_east)-1)
+                    self.team1 = current_round_east.pop(0)
+                    self.team2 = current_round_east.pop(len(current_round_east)-1)
 
                 elif east_finalist_chosen and west_finalist_chosen:
-                    self.home_team = current_round_west.pop(0)
-                    self.away_team = current_round_east.pop(0)
+                    self.team1 = current_round_west.pop(0)
+                    self.team2 = current_round_east.pop(0)
 
                 # Predicting
-                feature = self.load_feature(self.home_team, self.away_team)
-                winning_team = self.predict_value(feature[0])
+                feature1 = self.load_feature(self.team1, self.team2)
+                feature2 = self.load_feature(self.team2, self.team1)
+                winning_team = self.predict_value(feature1[0], feature2[0])
 
                 if east_finalist_chosen and west_finalist_chosen:
                     print winning_team + ' has won an NBA Championship!'
@@ -109,7 +156,7 @@ class Client:
                         print 'East Round Over, teams that move on ' + str(current_round_east) + '\n'
                         if len(current_round_east) == 1:
                             east_finalist_chosen = True
-                            print 'The ECF is ' + current_round_west[0] + '!\n'
+                            print 'The ECF is ' + current_round_east[0] + '!\n'
 
 
 
